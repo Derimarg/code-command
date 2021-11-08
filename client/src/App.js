d7eddaa08b1f7e928f28ca10fbf17bcd04c2c9c3
Derimar Gray <derimargray@gmail.com>
2021-11-07 20:42:27 -0600

    add client configuration to work with server

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
