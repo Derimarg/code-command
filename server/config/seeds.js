805c9fec9d0e257e2540acafea0be029dbc05d2c
Derimar Gray <derimargray@gmail.com>
2021-11-08 00:15:02 -0600

    add user seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,5 +1,32 @@
 const db = require("./connection");
-
+const { User }
 db.once("open", async () => {
-    
+    await User.deleteMany();
+
+    await User.create({
+        firstName: "Derimar",
+        lastName: "Gray",
+        email: "derimarg@email.com",
+        password: "password",
+        orders: []
+    });
+
+    await User.create({
+        firstName: "Rob",
+        lastName: "Atalla",
+        email: "Roba@email.com",
+        password: "password",
+        orders: []
+    });
+
+    await User.create({
+        firstName: "Audry",
+        lastName: "Ford",
+        email: "audryf@email.com",
+        password: "password",
+    });
+
+    console.log("users seeded");
+
+    process.exit();
 });
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { ApolloServer } = require("apollo-server express");
+const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
 // add typeDefs and resolvers
@@ -40,6 +40,7 @@ app.get("*", (req, res) => {
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
