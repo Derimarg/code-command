77d02450e61a190edc28460344d64bff7ecac6ca
Derimar Gray <derimargray@gmail.com>
2021-11-10 08:18:11 -0600

    add product typeDefs and fixed user typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,15 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type Product {
+    _id: _id
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+  }
+
   type User {
     _id: ID
     firstName: String
@@ -14,23 +23,25 @@ const typeDefs = gql`
   }
 
   type Query {
-      user: User
+    product(_id: ID!): Product
+    user: User
   }
 
   type Mutation {
-      addUser(
-          firstName: String!
-          lastName: String!
-          email: String!
-          password: String!
-      ): Auth
-      updateUser(
-          firstName:  String
-          lastName: String
-          email: String
-          password: String
-      ): User
-      login(email: String!): Auth
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): Auth
+    updateUser(
+      firstName: String
+      lastName: String
+      email: String
+      password: String
+    ): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String): Auth
   }
 `;
 
