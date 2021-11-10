d6525ac2243bb5bf87498ba7fc2b4656f5fabfaf
Derimar Gray <derimargray@gmail.com>
2021-11-10 09:13:48 -0600

    add category model

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const User = require("./User");
 const Product = require("./Product");
+const Category = require("./Category");
 
-module.exports = { User, Product };
+module.exports = { User, Product, Category };
