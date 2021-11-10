d7715ce239421b8d6f3474cdc4e4e73e49fac745
Derimar Gray <derimargray@gmail.com>
2021-11-10 08:45:45 -0600

    add  product resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,27 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
-
+const { User, Product } = require("../models");
 const { signToken } = require("../utils/auth");
-const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 
 const resolvers = {
   Query: {
+    products: async (parent, { name }) => {
+      const params = {};
+
+      if (name) {
+        params.name = {
+          $regex: name,
+        };
+      }
+
+      return await Product.find(params);
+    },
+    product: async (parent, { _id }) => {
+      return await Product.findById(_id);
+    },
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id);
+        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
 
         return user;
       }
@@ -18,7 +31,7 @@ const resolvers = {
   },
   Mutation: {
     addUser: async (parent, args) => {
-      const user = await User.create(args.user);
+      const user = await User.create(args);
       const token = signToken(user);
 
       return { token, user };
@@ -32,17 +45,26 @@ const resolvers = {
 
       throw new AuthenticationError("Not logged in");
     },
+    updateProduct: async (parent, { _id, quantity }) => {
+      const decrement = Math.abs(quantity) * -1;
+
+      return await Product.findByIdAndUpdate(
+        _id,
+        { $inc: { quantity: decrement } },
+        { new: true }
+      );
+    },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError("Incorrect Credentials");
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError("INcorrect credentials");
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
