a76804b4b256208cbe693bb361342aece8c4ce1d
Derimar Gray <derimargray@gmail.com>
2021-11-10 09:40:29 -0600

    add order resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User, Product, Category } = require("../models");
+const { User, Product, Category, Order } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
@@ -32,7 +32,21 @@ const resolvers = {
           populate: "category",
         });
 
+        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+
         return user;
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
+    order: async (parent, { _id }, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate({
+          path: "orders.products",
+          populate: "category",
+        });
+
+        return user.orders.id(_id);
       }
 
       throw new AuthenticationError("Not logged in");
@@ -44,6 +58,20 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
+    },
+    addOrder: async (parent, { products }, context) => {
+      console.log(context);
+      if (context.user) {
+        const order = new Order({ products });
+
+        await User.findByIdAndUpdate(context.user._id, {
+          $push: { orders: order },
+        });
+
+        return order;
+      }
+
+      throw new AuthenticationError("Not logged in");
     },
     updateUser: async (parent, args, context) => {
       if (context.user) {
