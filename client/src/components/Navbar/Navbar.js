05ddacad4af501722cae6525c343d7615c089c3a
Derimar Gray <derimargray@gmail.com>
2021-11-11 16:34:45 -0600

    add authentication to navbar routes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -14,6 +14,7 @@ import {
   NavBtnLink,
 } from "./Navbar.elements";
 import { Button } from "../../globalStyles";
+import Auth from "../../utils/auth";
 
 export default function Navbar() {
   const [click, setClick] = useState(false);
@@ -36,6 +37,86 @@ export default function Navbar() {
 
   window.addEventListener("resize", showButton);
 
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <>
+          <NavItem>
+            <NavLinks to="/" onClick={closeMobileMenu}>
+              Home
+            </NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="/orderHistory" onClick={closeMobileMenu}>
+              Order History
+            </NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="/courses" onClick={closeMobileMenu}>
+              Courses
+            </NavLinks>
+          </NavItem>
+          <NavItemBtn>
+            {button ? (
+              <NavBtnLink to="/">
+                <Button primary>LOGOUT</Button>
+              </NavBtnLink>
+            ) : (
+              <NavBtnLink to="/" onClick={() => Auth.logout()}>
+                <Button fontBig primary onClick={closeMobileMenu}>
+                  LOGOUT
+                </Button>
+              </NavBtnLink>
+            )}
+          </NavItemBtn>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <NavItem>
+            <NavLinks to="/" onClick={closeMobileMenu}>
+              Home
+            </NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="/services" onClick={closeMobileMenu}>
+              Services
+            </NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="/contact" onClick={closeMobileMenu}>
+              Contact Us
+            </NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="/courses" onClick={closeMobileMenu}>
+              Courses
+            </NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="/login" onClick={closeMobileMenu}>
+              Login
+            </NavLinks>
+          </NavItem>
+          <NavItemBtn>
+            {button ? (
+              <NavBtnLink to="/signup">
+                <Button primary>Sign Up</Button>
+              </NavBtnLink>
+            ) : (
+              <NavBtnLink to="/signup">
+                <Button fontBig primary onClick={closeMobileMenu}>
+                  Sign Up
+                </Button>
+              </NavBtnLink>
+            )}
+          </NavItemBtn>
+        </>
+      );
+    }
+  }
+
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
@@ -48,44 +129,7 @@ export default function Navbar() {
               {click ? <FaTimes /> : <FaBars />}
             </MobileIcon>
             <NavMenu onClick={handleClick} click={click}>
-              <NavItem>
-                <NavLinks to="/" onClick={closeMobileMenu}>
-                  Home
-                </NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="/services" onClick={closeMobileMenu}>
-                  Services
-                </NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="/contact" onClick={closeMobileMenu}>
-                  Contact Us
-                </NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="/courses" onClick={closeMobileMenu}>
-                  Courses
-                </NavLinks>
-              </NavItem>
-              <NavItem>
-                <NavLinks to="/login" onClick={closeMobileMenu}>
-                  Login
-                </NavLinks>
-              </NavItem>
-              <NavItemBtn>
-                {button ? (
-                  <NavBtnLink to="/signup">
-                    <Button primary>Sign Up</Button>
-                  </NavBtnLink>
-                ) : (
-                  <NavBtnLink to="/signup">
-                    <Button fontBig primary onClick={closeMobileMenu}>
-                      Sign Up
-                    </Button>
-                  </NavBtnLink>
-                )}
-              </NavItemBtn>
+              {showNavigation()}
             </NavMenu>
           </NavbarContainer>
         </Nav>
