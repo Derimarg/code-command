bd5fd6f6e9af5f3f168840f0bf8635ffc22f452d
Derimar Gray <derimargray@gmail.com>
2021-11-11 11:24:07 -0600

    update user queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -5,6 +5,17 @@ export const QUERY_USER = gql`
     user {
       firstName
       lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
       }
     }
   }
