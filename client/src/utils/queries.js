fbf520d5e37bb9b0a3427308f68c9247445f6648
Derimar Gray <derimargray@gmail.com>
2021-11-11 11:36:56 -0600

    add product and category queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -20,3 +20,43 @@ export const QUERY_USER = gql`
     }
   }
 `;
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      quantity
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
