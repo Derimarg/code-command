c6dc7118e86bf24cb10c5086f92f1fcecc3add8a
georgedmendoza <georgedmendoza13@gmail.com>
2021-11-11 19:16:27 -0600

    checkout cart layout

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import CartItems from '../CartItems';
+import { productData } from '../Products';
+import Auth from '../../utils/auth';
+import { TOGGLE_CART } from '../../utils/actions';
+
+const Cart = () => {
+
+    function toggleCart() {
+        dispatchEvent({ type: TOGGLE_CART });
+    };
+
+    function calculateTotal() {
+        let sum = 0;
+        state.cart.forEach(item => {
+            sum += item.price * item.purchaseQuantity;
+        });
+        return sum.toFixed(2);
+    }
+
+    return(
+        // add style here
+        <span>placeholder</span>
+    )
+};
+
+export default Cart;
