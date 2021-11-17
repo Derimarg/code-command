0ca098f2e462878c7e65dbfce25b63b7f087e63f
Derimar Gray <derimargray@gmail.com>
2021-11-16 19:18:52 -0600

    update routes and not found page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { withRouter } from "react-router";
 import "./App.css";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
@@ -19,6 +20,7 @@ import Detail from "./pages/Detail/Detail";
 import Contact from "./pages/Contact/Contact";
 import OrderHistory from "./pages/OrderHistory/OrderHistory";
 import Success from "./pages/Success/Success";
+import NotFound from "./pages/NotFound/NotFound";
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -32,6 +34,36 @@ const client = new ApolloClient({
   uri: "/graphql",
 });
 
+const Main = withRouter(({ location }) => {
+  return (
+    <div>
+      {location.pathname !== "/login" && location.pathname !== "/signup" && (
+        <Navbar />
+      )}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/courses" component={Courses} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/success" component={Success} />
+        <Route exact path="/orderHistory" component={OrderHistory} />
+        <Route exact path="/products/:id" component={Detail} />
+        <Route component={NotFound} />
+      </Switch>
+
+      {location.pathname !== "/login" && location.pathname !== "/signup" && location.pathname !== "" && (
+        <Cartbar />
+      )}
+      {location.pathname !== "/login" && location.pathname !== "/signup" && (
+        <Footer />
+      )}
+    </div>
+  );
+});
+
 function App() {
   return (
     <ApolloProvider client={client}>
@@ -40,26 +72,8 @@ function App() {
           <Provider store={store}>
             <GlobalStyle />
             <ScrollToTop />
-            <Switch>
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/login" component={Login} />
-              <>
-                <Navbar />
 
-                <Route exact path="/" component={Home} />
-                <Route exact path="/services" component={Services} />
-                <Route exact path="/courses" component={Courses} />
-                <Route exact path="/contact" component={Contact} />
-                <Route exact path="/checkout" component={Checkout} />
-                <Route exact path="/success" component={Success}/>
-                <Route exact path="/orderHistory" component={OrderHistory} />
-                <Route exact path="/products/:id" component={Detail} />
-                {/* <Route component={NotFound} /> */}
-                <Cartbar />
-
-                <Footer />
-              </>
-            </Switch>
+            <Main />
           </Provider>
         </div>
       </Router>
