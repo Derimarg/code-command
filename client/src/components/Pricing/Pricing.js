966fdbcd132f89fddb3854cfa4d1e4d907fa3feb
audryf <84256033+audryf@users.noreply.github.com>
2021-11-17 19:09:45 -0600

    update subscription area, need to display only 3

--- client/src/components/Pricing/Pricing.js
+++ client/src/components/Pricing/Pricing.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "../../globalStyles";
 import { QUERY_PRODUCTS } from "../../utils/queries";
-import { GiCrystalBars } from "react-icons/gi";
-import { GiCutDiamond, GiRock } from "react-icons/gi";
+import { MsgContainer } from "../../components/Products/ProductsElements";
+// import { GiCrystalBars, GiCutDiamond } from "react-icons/gi";
+import { GiRock } from "react-icons/gi";
 import { IconContext } from "react-icons/lib";
+import { useSelector, useDispatch } from "react-redux";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from "@apollo/react-hooks";
+import { idbPromise } from "../../utils/helpers";
 import {
   PricingSection,
   PricingWrapper,
@@ -20,29 +25,86 @@ import {
 } from "./Pricing.elements";
 
 function Pricing() {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const { currentCategory } = state;
+
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+
+      data.products.forEach((product) => {
+        idbPromise("products", "put", product);
+      });
+    } else if (!loading) {
+      setTimeout(() => {
+        idbPromise("products", "get").then((products) => {
+          dispatch({
+            type: UPDATE_PRODUCTS,
+            products: products,
+          });
+        });
+      }, 5000);
+    }
+  }, [data, loading, dispatch]);
+
+  function filterCourses() {
+    if (!currentCategory) {
+      return state.products;
+    }
+
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
+  }
+
   return (
     <IconContext.Provider value={{ color: "#a9b3c1", size: 64 }}>
       <PricingSection>
         <PricingWrapper>
           <PricingHeading>Our Services</PricingHeading>
           <PricingContainer>
-            <PricingCard to="/sign-up">
-              <PricingCardInfo>
-                <PricingCardIcon>
-                  <GiRock />
-                </PricingCardIcon>
-                <PricingCardPlan>Starter Pack</PricingCardPlan>
-                <PricingCardCost>$15.99</PricingCardCost>
-                <PricingCardLength>per month</PricingCardLength>
-                <PricingCardFeatures>
-                  <PricingCardFeature>100 New Users</PricingCardFeature>
-                  <PricingCardFeature>$10,000 Budget</PricingCardFeature>
-                  <PricingCardFeature>Retargeting analytics</PricingCardFeature>
-                </PricingCardFeatures>
-                <Button primary>Choose Plan</Button>
-              </PricingCardInfo>
-            </PricingCard>
-            <PricingCard to="/sign-up">
+            {state.products.length ? (
+              <>
+                {filterCourses().map((product) => (
+                  <PricingCard to="/sign-up">
+                    <PricingCardInfo>
+                      <PricingCardIcon>
+                        <GiRock />
+                      </PricingCardIcon>
+                      <PricingCardPlan>{product.name}</PricingCardPlan>
+                      <PricingCardCost>${product.price}</PricingCardCost>
+                      <PricingCardLength>per month</PricingCardLength>
+                      <PricingCardFeatures>
+                        <PricingCardFeature>100 New Users</PricingCardFeature>
+                        <PricingCardFeature>$10,000 Budget</PricingCardFeature>
+                        <PricingCardFeature>Retargeting analytics</PricingCardFeature>
+                      </PricingCardFeatures>
+                      <Button primary>View Plan</Button>
+                    </PricingCardInfo>
+                  </PricingCard>
+                ))}
+              </>
+            ) : (
+              <MsgContainer>
+                <h3>You haven't added any products yet!</h3>
+              </MsgContainer>
+            )}
+
+          </PricingContainer>
+        </PricingWrapper>
+      </PricingSection>
+    </IconContext.Provider>
+  );
+}
+export default Pricing;
+{/* <PricingCard to="/sign-up">
               <PricingCardInfo>
                 <PricingCardIcon>
                   <GiCrystalBars />
@@ -73,11 +135,4 @@ function Pricing() {
                 </PricingCardFeatures>
                 <Button primary>Choose Plan</Button>
               </PricingCardInfo>
-            </PricingCard>
-          </PricingContainer>
-        </PricingWrapper>
-      </PricingSection>
-    </IconContext.Provider>
-  );
-}
-export default Pricing;
+            </PricingCard> */}
