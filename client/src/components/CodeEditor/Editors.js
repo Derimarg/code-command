dd21a0daf6ded6edc13f7d121b6d353a2d605109
Derimar Gray <derimargray@gmail.com>
2021-11-22 11:17:15 -0600

    add editor header

--- client/src/components/CodeEditor/Editors.js
+++ client/src/components/CodeEditor/Editors.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Editor from "@monaco-editor/react";
+import EditorHeading from "./EditorHeading";
 
 const Editors = (prop) => {
   const { value, lang, setValue, displayName } = prop;
@@ -10,11 +11,17 @@ const Editors = (prop) => {
   return (
     <td>
       <div className="editor">
-        <Editor width="100%" height="50vh" 
-        defaultLanguage={lang}
-        defaultValue = {value}
-        theme="vs-dark" />
-        onChange={handleEditorChange}
+        <EditorHeading name={displayName} />
+      </div>
+      <div className="editor">
+        <Editor
+          width="100%"
+          height="50vh"
+          defaultLanguage={lang}
+          defaultValue={value}
+          theme="vs-dark"
+          onChange={handleEditorChange}
+        />
       </div>
     </td>
   );
